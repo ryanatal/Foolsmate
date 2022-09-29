f04ec42230de41aebe9144644b4e95086ff6b641
Ryan Atallah <104354805+ryanatal@users.noreply.github.com>
2022-09-29 22:06:21 +0300

    Adding the tester comment to try out branching

--- Foolsmate.c
+++ Foolsmate.c
@@ -9,7 +9,7 @@
 
 /* variables *************************************************************/
 
-
+//adding a tester comment to see if this works
 /*TO DO
 
 -timer for each player and compare time if draw  (check hello.c)
